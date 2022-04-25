9c5915d5a55c43a140baac01ed97a27288221832
shiofrui <tomo170119@gmail.com>
2022-04-26 02:15:30 +0900

    edit

--- main.js
+++ main.js
@@ -1,137 +1,172 @@
-const random = (n) => Math.floor(Math.random() * n);
-const answer = wordList[random(wordList.length)];
+OVERLAP_SIGNS = ['〇', '×', '△', '□', '◇', '▽'];
 
-let startTime;
-let gameStatus = "await";
-const overlapSign = ['〇', '×', '△', '□', '◇', '▽'];
-const overlapList = [...answer].filter((v, i, array) => !(array.indexOf(v) == i));
-const noMatchList = [];
-
-let gameBoard;
-let displayRow;
-let overlapRow;
-let entryBox;
-
-const timer = document.getElementById('timer');
-const noMatchRow = document.getElementById('no-match-row');
-const startButton = document.getElementById('start-button');
-const searchButton = document.getElementById("search-button");
-
-const initBoard = () => {
-  gameBoard = document.getElementById("display-row");
-  displayRow = document.getElementById("display-row");
-  for (let letter of answer) {
-    let box = document.createElement("div");
-    box.className = "letter-box";
-    displayRow.appendChild(box);
-  }
-  overlapRow = document.getElementById("overlap-row");
-  for (let letter of answer) {
-    let box = document.createElement("div");
-    box.className = "letter-box";
-    if (overlapList.includes(letter)) {
-      box.innerText = overlapSign[overlapList.indexOf(letter)];
-    }
-    overlapRow.appendChild(box);
-  }
-  const entryRow = document.getElementById("entry-row");
-  entryBox = document.createElement("div");
-  entryBox.className = "letter-box";
-  entryRow.appendChild(entryBox);
+let game;
+window.onload = () => {
+  game = new Game();
 }
 
-const initKeyBoard = () => {
-  const keyboard = document.getElementById("keyboard");
-  keyboard.addEventListener("click", (e) => {
-    const target = e.target;
-    let key = target.textContent;
-    if (key === "Del") {
-      key = "Backspace"
+class Game {
+  constructor() {
+    this.timer = new Timer();
+    this.isInPlay = false;
+    this.isOver = false;
+
+    this.answer = wordList[Math.floor(Math.random() * wordList.length)];
+    this.noMatchList = [];
+
+    this.gameBoard = document.getElementById("game-board");
+    this.displayRow = document.getElementById("display-row");
+    this.noMatchRow = document.getElementById("no-match-row");
+    this.entryBox = document.getElementById("entry-box");
+    this.keyBoard = document.getElementById("keyboard");
+    this.startButton = document.getElementById("start-button");
+    this.reloadButton = document.getElementById("reload-button");
+    this.searchButton = document.getElementById("search-button");
+
+    this.createBoard();
+    this.keyBoard.onclick = this.adaptKeyboard;
+    document.onkeydown = this.acceptKey.bind(this);
+    this.startButton.onclick = this.start.bind(this);
+    this.reloadButton.onclick = this.reload.bind(this);
+    this.searchButton.onclick = this.search.bind(this);
+  }
+
+  start() {
+    this.timer.start();
+    this.isInPlay = true;
+    this.startButton.style.display = "none";
+    this.gameBoard.style.visibility = "visible";
+  }
+
+  createBoard() {
+    for (let letter of this.answer) {
+      let box = document.createElement("div");
+      box.className = "letter-box";
+      this.displayRow.appendChild(box);
     }
+
+    const overlapRow = document.getElementById("overlap-row");
+    const overlapList = [...this.answer].filter((v, i, array) => !(array.indexOf(v) == i));
+    for (let letter of this.answer) {
+      let box = document.createElement("div");
+      box.className = "letter-box";
+      if (overlapList.includes(letter)) {
+        box.innerText = OVERLAP_SIGNS[overlapList.indexOf(letter)];
+      }
+      overlapRow.appendChild(box);
+    }
+  }
+
+  adaptKeyboard(e) {
+    const input = e.target.textContent;
+
+    let key;
+    if (input === "Del") {
+      key = "Backspace"
+    } else {
+      key = input;
+    }
+
     const keyDownEvent = new KeyboardEvent("keydown", {
       'key': key
     });
+
     document.dispatchEvent(keyDownEvent);
-  });
-}
+  }
 
-window.onload = () => {
-  initBoard();
-  initKeyBoard();
-}
+  acceptKey(e) {
+    if (e.key == " " && !this.isInPlay) {
+      this.start.bind(this)();
+    }
 
-const inputKey = (key) => {
-  if (answer.includes(key)) {
-    [...answer].forEach((letter, i) => {
+    if (e.key == " " && this.isOver) {
+      this.reload.bind(this)();
+    }
+
+    if (!this.isInPlay) return;
+
+    if (e.key.match('^[a-zA-Z]{1}$')) {
+      this.entryBox.innerText = e.key.toUpperCase();
+    } else if (e.key == 'Backspace') {
+      this.entryBox.innerText = '';
+    } else if (e.key == 'Enter') {
+      this.checkKey(this.entryBox.innerText);
+      this.checkStatus();
+      this.entryBox.innerText = '';
+    }
+  }
+
+  checkKey(key) {
+    if (this.answer.includes(key)) {
+      this.fillByKey(key);
+    } else if (!this.noMatchList.includes(key)) {
+      const box = document.createElement("div");
+      box.className = "letter-box";
+      box.innerText = key;
+      this.noMatchRow.appendChild(box);
+      this.noMatchList.push(key);
+    }
+  }
+
+  checkStatus() {
+    const isCompleted = [...this.answer].every((letter, i) => {
+      return this.displayRow.children[i].innerText == letter;
+    });
+
+    if (this.noMatchList.length >= 7 || isCompleted) {
+      this.isOver = true;
+      this.timer.stop();
+      this.fillAll();
+      this.reloadButton.style.visibility = "visible";
+      this.searchButton.style.visibility = "visible";
+    }
+  }
+
+  fillByKey(key) {
+    [...this.answer].forEach((letter, i) => {
       if (letter != key) return;
-      displayRow.children[i].innerText = letter;
+      this.displayRow.children[i].innerText = letter;
     });
-  } else if (!noMatchList.includes(key)) {
-    noMatchList.push(key);
-    const box = document.createElement("div");
-    box.className = "letter-box";
-    box.innerText = key;
-    noMatchRow.appendChild(box);
   }
-}
 
-searchButton.onclick = () => {
-  window.open("https://www.google.com/search?q=" + answer.toLowerCase());
-}
-
-const checkGameStatus = () => {
-  const isCompleted = [...answer].every((letter, i) => {
-    return displayRow.children[i].innerText == letter;
-  });
-  if (noMatchList.length >= 7 || isCompleted) {
-    gameStatus = "over";
-    [...answer].forEach((letter, i) => {
-      displayRow.children[i].innerText = letter;
+  fillAll() {
+    [...this.answer].forEach((letter, i) => {
+      this.displayRow.children[i].innerText = letter;
     });
-    searchButton.style.visibility = "visible";
+  }
+
+  reload() {
+    location.reload();
+  }
+
+  search() {
+    window.open("https://www.google.com/search?q=" + this.answer.toLowerCase());
   }
 }
 
-document.addEventListener('keydown', (e) => {
-  const pressedKey = e.key;
-  if (gameStatus == "await") {
-    if (pressedKey == ' ' && gameStatus == "await") {
-      startGame();
+class Timer {
+  constructor() {
+    this.isActive = false;
+  }
+
+  start() {
+    this.isActive = true;
+    this.startTime = Date.now();
+    this.countUp();
+  }
+
+  countUp() {
+    const nowTime = new Date(Date.now() - this.startTime);
+    const min = String(nowTime.getMinutes()).padStart(2, 0);
+    const sec = String(nowTime.getSeconds()).padStart(2, 0);
+    const ms = String(nowTime.getMilliseconds()).padStart(3, 0);
+    document.getElementById("timer").innerText = `${min}:${sec}.${ms}`;
+    if (this.isActive) {
+      setTimeout(this.countUp.bind(this), 10);
     }
-  } else if (gameStatus == "playing") {
-    if (pressedKey.match('^[a-zA-Z]{1}$')) {
-      entryBox.innerText = pressedKey.toUpperCase();
-    } else if (pressedKey == 'Backspace') {
-      entryBox.innerText = '';
-    } else if (pressedKey == 'Enter') {
-      inputKey(entryBox.innerText);
-      checkGameStatus();
-      entryBox.innerText = '';
-    }
-  } else if (gameStatus == "over") {
-
-  } else {
-
   }
-});
 
-const countUp = () => {
-  const nowTime = new Date(Date.now() - startTime);
-  const min = String(nowTime.getMinutes()).padStart(2, 0);
-  const sec = String(nowTime.getSeconds()).padStart(2, 0);
-  const ms = String(nowTime.getMilliseconds()).padStart(3, 0);
-  timer.innerText = `${min}:${sec}.${ms}`;
-  if (gameStatus != "over") {
-    setTimeout(countUp, 10);
+  stop() {
+    this.isActive = false;
   }
 }
-
-const startGame = () => {
-  gameStatus = "playing";
-  document.getElementById("game-board").style.visibility = "visible";
-  startButton.style.visibility = "hidden";
-  startTime = Date.now();
-  countUp();
-}
-
-startButton.addEventListener('click', startGame);
